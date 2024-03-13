f22de87ceeda641f3093311e9e1d7f7b04bb4c80
jopdel <opdelochtjohn@gmail.com>
2024-03-13 15:08:24 +0100

    PDF generated file SouthAfrica theme, with qr code.

--- oefthema/exportpdf.php
+++ oefthema/exportpdf.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 ob_end_clean();
-require  'vendor/autoload.php'; 
+require  'vendor/autoload.php';
 
 /* Assignment:
 Try using styling, like font names and colors
@@ -14,14 +14,135 @@ $name = $_SESSION['name'];
 $email = $_SESSION['email'];
 $tel = $_SESSION['tel'];
 $tday_name = $_SESSION['tday_name'];
+$tday_date = $_SESSION['tday_date'];
+$locname = $_SESSION['locname'];
 
 // Create a new instance of DOMPDF class and allow remote images in content !!! very important when handling images in our dompdf! 
-$dompdf  =  new \Dompdf\Dompdf(array('enable_remote'  =>  true));   
+$dompdf  =  new \Dompdf\Dompdf(array('enable_remote'  =>  true));
+
+$options = new \Dompdf\Options();
+$options->set('chroot', realpath(''));
+$dompdf = new \Dompdf\Dompdf($options);
+// $dompdf->loadHtml('<img src="assets/code.jpeg" alt="qrcode" style="width: 180px;">');
+
 // Define the HTML content  
-$html  =  "<h1>Hello $name</h1>
-<img src='https://www.syntrapxl.be/themes/custom/sassy/assets/images/syntra/logo.svg'>
-<p>U heeft een themadag geboekt ($tday_name). U heeft een e-mailadres: $email. U heeft een telefoonnummer: $tel.</p>";  
- // Load the HTML content to DOMPDF
+$html = "
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+    <style>
+    @page {
+        margin: 0;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 18px;
+
+    }
+    .header {
+        width: 100%;
+        height: 300px;
+        background-image: url('assets/zuidafrika.jpg');
+        background-size: cover;
+        background-position: center;
+    }
+    .h3-colored-box {
+        background-color: #FF9A44; 
+        padding: 20px; 
+        text-align: center;
+    }    
+    h3 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        font-size: 36px;
+        text-transform: uppercase;
+        font-weight: 700;
+    }
+    h4 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        font-size: 20px;
+    }
+    .dateloc {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        font-size: 28px;
+        text-transform: uppercase;
+        font-weight: 700;
+    }
+    p {
+        text-align: center;
+    }
+    th {
+        text-align: left;
+    }
+    .center-table {
+        margin-left: auto;
+        margin-right: auto;
+        width: 65%; 
+    }    
+    .logo {
+        text-align: center;
+        margin-top: 80px;
+    }
+    </style>
+</head>
+<body>
+    <div class='header'>
+    </div>
+    <div class='day'>
+        <h3 class='h3-colored-box'>$tday_name</h3>
+    </div>
+    <div class='dateloc'>
+        $tday_date<br>$locname
+    </div>
+    <br>
+    <br>
+    <h4>Beste $name, <br>bedankt voor uw boeking!</h4>
+    <h4>Details:</h4>
+    <div class='details center-table'>
+        <table>
+             <tr>
+                <td rowspan='6'>
+                <img src='assets/code.jpeg' alt='qrcode' style='width: 120px; margin-right: 20px;'>
+                </td>
+                <th>Themadag</th>
+                <td>$tday_name</td>
+            </tr>       
+            <tr>
+                <th>Naam</th>
+                <td>$name</td>
+            </tr>
+            <tr>
+                <th>Datum</th>
+                <td>$tday_date</td>
+            </tr>
+            <tr>
+                <th width='140px'>Locatienaam</th>
+                <td>$locname</td>
+            </tr>
+            <tr>
+                <th>E-mail</th>
+                <td>$email</td>
+            </tr>
+            <tr>
+                <th>Telefoon</th>
+                <td>$tel</td>
+            </tr>
+        </table>
+    </div>
+    <div class='logo'>
+        <img src='assets/logo.svg' alt='logo' style='width: 180px;'></p> 
+    </div>
+</body>
+</html>
+";
+// Load the HTML content to DOMPDF
 $dompdf->loadHtml($html);
 // (Optional) Setup the paper size and orientation
 $dompdf->setPaper('A4', 'portrait');
@@ -30,4 +151,4 @@ $dompdf->render();
 // Output the generated PDF to Browser
 // generate random filename
 $filename = md5(microtime()) . '_themadag.pdf';
-$dompdf->stream($filename,  array("Attachment"  =>  1)); 
+$dompdf->stream($filename,  array("Attachment"  =>  0));
--- oefthema/submit.php
+++ oefthema/submit.php
@@ -2,14 +2,16 @@
 <?php include 'global.php'; ?>
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Themadag</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-  <?php 
+</head>
+
+<body>
+    <?php
     // print_r($_POST);
     // assign values to variables from POST
     $name = $_POST['name'];
@@ -23,7 +25,8 @@
     $_SESSION['tel'] = $tel;
     $_SESSION['tday_id'] = $tday_id;
 
-    function saveStringToFile($name, $email, $tel, $locaties) {
+    function saveStringToFile($name, $email, $tel, $locaties)
+    {
         // open or create a new file and append to it
         $file = fopen("bookings.csv", "a");
         // provice a prefered format for the date object
@@ -33,8 +36,9 @@
         fwrite($file, $string);
         fclose($file);
     }
-    
-    function get_tday_name($tday_id) {
+
+    function get_tday_name($tday_id)
+    {
         global $conn;
         $sql = "SELECT name FROM tdays WHERE id = $tday_id";
         $result = $conn->query($sql);
@@ -43,38 +47,75 @@
         return $tday_name;
     }
 
+    function get_tday_date($tday_id)
+    {
+        global $conn;
+        $sql = "SELECT date FROM tdays WHERE id = $tday_id";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $tday_date = $row['date'];
+        return $tday_date;
+    }
+
+    function get_locname($tday_id)
+    {
+        global $conn;
+        $sql = "SELECT
+        tdays.id,
+        tdays.`name` AS themename,
+        locations.`name` AS locname
+        FROM tdays
+        JOIN locations ON locations.id = tdays.location_id";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $locname = $row['locname'];
+        return $locname;
+    }
+
+
+
     saveStringToFile($name, $email, $tel, $tday_id);
-    store ($name, $email, $tel, $tday_id);
+    store($name, $email, $tel, $tday_id);
 
     // get themadag name from database based on tday_id;
     $tday_name = get_tday_name($tday_id);
     // store the themaday name in session
     $_SESSION['tday_name'] = $tday_name;
+    // get themadag date from database based on tday_id;
+    $tday_date = get_tday_date($tday_id);
+    // store the date in session
+    $_SESSION['tday_date'] = $tday_date;
+    // get location name from database based on tday_id;
+    $locname = get_locname($tday_id);
+    // store the location name in session
+    $_SESSION['locname'] = $locname;
 
-   ?>
-  <div class="container">
-       <div class="row">
+
+    ?>
+    <div class="container">
+        <div class="row">
             <div class="col-md">
 
-            <h1>Bedankt voor uw inschrijving, <?php echo $name; ?></h1>
-            <h3>Wij verwachten u in <?php echo $tday_name; ?></h3>
-            <p>Hier is de gegevens die u heeft ingevuld:</p>
-            <ul>
-                <li>Naam: <?php echo $name; ?></li>
-                <li>Email: <?php echo $email; ?></li>
-                <li>Telefoon: <?php echo $tel; ?></li>
-                <li>Locatie: <?php echo $tday_name; ?></li>
-            </ul>
+                <h1>Bedankt voor uw inschrijving, <?php echo $name; ?></h1>
+                <h3>Wij verwachten u in <?php echo $tday_name; ?></h3>
+                <p>Hier is de gegevens die u heeft ingevuld:</p>
+                <ul>
+                    <li>Naam: <?php echo $name; ?></li>
+                    <li>Email: <?php echo $email; ?></li>
+                    <li>Telefoon: <?php echo $tel; ?></li>
+                    <li>Locatie: <?php echo $tday_name; ?></li>
+                </ul>
 
-            <a class='btn btn-primary' href="index.php">Nieuwe inschrijving</a>
-            <a class='btn btn-dark' href="print.php">Inschrijving afdrukken</a>
-            <a class='btn btn-danger' href="exportpdf.php">Export naar pdf</a>
+                <a class='btn btn-primary' href="index.php">Nieuwe inschrijving</a>
+                <a class='btn btn-dark' href="print.php">Inschrijving afdrukken</a>
+                <a class='btn btn-danger' href="exportpdf.php">Export naar pdf</a>
+            </div>
         </div>
     </div>
-</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 
 </body>
+
 </html>
